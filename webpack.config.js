0b17e51b0daec45c3d44ec3389d58faf6af5357d
Sergey Chivirda <si.chivirda@gmail.com>
2020-04-22 22:07:39 +0300

    Подключил webpack и основные модули

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: './index.js'
+    }
+}
