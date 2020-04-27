182b72c234a41987e1bf51a1c09aed5b4b7ffbe5
Sergey Chivirda <si.chivirda@gmail.com>
2020-04-27 23:30:22 +0300

    добавил обработку события клика для кнопки "Select Pharmacies"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import './css/style.css'
 import LegalEntity from './js/legalEntity'
 
+const selectPharmaciesButton = document.querySelector('#selectEntityButton')
+
 LegalEntity.showEntityes()
+
+selectPharmaciesButton.addEventListener('click', () => {
+    console.log('Select button clicked')
+})
